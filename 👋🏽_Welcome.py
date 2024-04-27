667f3520f7d236d691e2100be49b18b93db6ccd9
MAURICIO MANUEL F. BERGANCIA <mauriciomanuel.bergancia@wvsu.edu.ph>
2024-04-27 15:29:20 +0800

    Update 👋🏽_Welcome.py

--- 👋🏽_Welcome.py
+++ 👋🏽_Welcome.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 # Path to local image
-image_path = "D:\BSCS 2-A AI (2nd Sem)\Algorithms and Complexity\A-Star Search App\pages\contents\g1.jpg"
+image_path = "pages/media/g1.jpg"
 
 img = Image.open(image_path)
 
